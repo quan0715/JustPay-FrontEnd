8b85cd6ccae39f47ff61e36da9dc2ce7393d0351
quan0715 <quan787887@gmail.com>
2025-03-31 10:44:02 +0800

    remove unnessarry component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,7 @@
 "use client";
 // import { useState } from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import { Loader2 } from "lucide-react";
+
 import "@rainbow-me/rainbowkit/styles.css";
-import { getSession } from "next-auth/react";
-import { getToken } from "next-auth/jwt";
 import { AuthStatus } from "@/components/AuthStatus";
 
 export default function Home() {
